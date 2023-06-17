5ecdb2aed5826e242717590a73c73fb608fe8a26
hideki watabe <hideki.w.1971@gmail.com>
2023-06-17 19:47:31 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  MicroModal.init({
+    awaitCloseAnimation: true,
+    awaitOpenAnimation: true,
+    disableScroll: true
+  });
+  // 
+});
